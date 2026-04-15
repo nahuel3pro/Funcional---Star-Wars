cb39d59a57f1799a2251f745f7a7f6db7f190899
Nahuel <nahuel@localhost.localdomain>
2026-04-15 18:36:13 -0300

    Agregadas estructuras y funciones
    Se agregaron las estructuras para el TP, las funciones tieneBuenaMusica, esGrande, esCumplanieroMayorDeEdad, esAburrida y buenaFiesta. También una instancia de prueba con Gerardo

--- src/Library.hs
+++ src/Library.hs
@@ -3,3 +3,44 @@ import PdePreludat
 
 doble :: Number -> Number
 doble numero = numero + numero
+
+---- Pruebas
+gerardo :: Persona
+gerardo = UnaPersona "Gerardo" "Facha" 35
+fiestaGerardo :: Fiesta
+fiestaGerardo = UnaFiesta gerardo ["Nahuel", "Lucas", "Micaela", "Tomás", "Matías"] "rock"
+
+type Nombre = String
+type Apodo = String
+type Genero = String
+type Edad = Number
+type Cantidad = Number
+---- Estructuras
+
+data Persona = UnaPersona{
+	nombre :: Nombre,
+	apodo :: Apodo,
+	edad :: Edad
+}
+
+data Fiesta = UnaFiesta{
+	cumpleaniero :: Persona,
+	listaInv :: [Nombre],
+	generoMusica :: Genero
+}
+
+---- Funciones
+tieneBuenaMusica :: Fiesta -> Bool
+tieneBuenaMusica fiesta = ((generoMusica fiesta) == "rock") || ((generoMusica fiesta) == "regueton viejo")
+
+esGrande :: Fiesta -> Bool
+esGrande fiesta = ((>=3).length.listaInv) fiesta
+
+esCumplanieroMayorDeEdad :: Fiesta -> Bool
+esCumplanieroMayorDeEdad fiesta = (edad.cumpleaniero) fiesta >= 18
+
+esAburrida :: Fiesta -> Bool
+esAburrida fiesta = (generoMusica fiesta == "clasico") && ((not.esGrande) fiesta)
+
+buenaFiesta :: Fiesta -> Bool
+buenaFiesta fiesta = (tieneBuenaMusica fiesta) && (esGrande fiesta) && (esCumplanieroMayorDeEdad fiesta)
