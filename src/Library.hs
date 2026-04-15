0da2d83adc02383aacf9e6f02de34f36b4bcc8ca
ivan Lopez <ivlopez2001@gmail.com>
2026-04-15 16:21:39 -0300

    adelante un poquito

--- src/Library.hs
+++ src/Library.hs
@@ -1,5 +1,33 @@
 module Library where
-import PdePreludat
+--import PdePreludat
 
-doble :: Number -> Number
-doble numero = numero + numero
+type Nombre = String
+type AnioNacimiento = Int
+type EstiloMusica = String
+
+
+
+
+-- modelo persona
+
+data Persona = Persona {
+    nombre :: String,
+    anioNacimiento :: Int
+} deriving (Eq, Show)
+
+--modelo fiesta
+
+data Fiesta = Fiesta {
+    cumpleaniero :: Persona,
+    invitados :: [Persona],
+    estiloMusica :: String
+} deriving (Eq, Show)
+
+juan :: Persona
+juan = Persona "Juan" 1995
+
+esMayor :: Persona -> Persona -> Bool
+esMayor persona persona2 = anioNacimiento persona < anioNacimiento persona2
+
+ponerApodo :: Persona -> String -> Persona
+ponerApodo persona apodo = persona { nombre = apodo }
