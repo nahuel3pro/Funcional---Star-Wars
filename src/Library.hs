7a12a313c299cd6663dab18c58573f246bb872bf
ivan Lopez <you@example.com>
2026-04-21 11:16:39 -0300

    comente para que punto es cada funcion

--- src/Library.hs
+++ src/Library.hs
@@ -1,6 +1,7 @@
 module Library where
 import PdePreludat
 
+
 doble :: Number -> Number
 doble numero = numero + numero
 
@@ -15,50 +16,63 @@ type Apodo = String
 type Genero = String
 type Anio = Number
 type Cantidad = Number
----- Estructuras
+---- 
 
 data Persona = UnaPersona{
 	nombre :: Nombre,
 	apodo :: Apodo,
 	anioNacimiento :: Anio
-}
+}deriving(Show,Eq)
 
 data Fiesta = UnaFiesta{
 	cumpleaniero :: Persona,
 	listaInvitados :: [Nombre],
 	generoMusica :: Genero
-}
+}deriving(Show,Eq)
 
 ---- Funciones
+
+--punto 2
 ponerApodo :: Apodo -> Persona -> Persona
 ponerApodo apodoPersona persona = persona {apodo = apodoPersona}
 
+
+-- Punto 1
 esMayor :: Persona -> Persona -> Bool
 esMayor persona persona2 = anioNacimiento persona < anioNacimiento persona2
 
+-- punto 4
 tieneBuenaMusica :: Fiesta -> Bool
 tieneBuenaMusica fiesta = ((generoMusica fiesta) == "rock") || ((generoMusica fiesta) == "regueton viejo")
 
+-- punto 3
 esGrande :: Fiesta -> Bool
 esGrande fiesta = ((>=10).length.listaInvitados) fiesta
 
 esCumplanieroMayorDeEdad :: Fiesta -> Bool
 esCumplanieroMayorDeEdad fiesta = (2026 - (anioNacimiento.cumpleaniero) fiesta) >= 18
 
+--punto 7
 esAburrida :: Fiesta -> Bool
 esAburrida fiesta = (generoMusica fiesta == "clasico") && ((not.esGrande) fiesta)
 
+
+--punto 8
 buenaFiesta :: Fiesta -> Bool
 buenaFiesta fiesta = (tieneBuenaMusica fiesta) && (esGrande fiesta) && (esCumplanieroMayorDeEdad fiesta)
 
+--punto 6 
 invitar :: Fiesta -> Persona -> Fiesta
 invitar fiesta persona = fiesta{listaInvitados = (nombre persona) : (listaInvitados fiesta)}
 
+-- punto 5
 laMayor :: Persona -> Persona -> Persona
 laMayor persona persona2
     | esMayor persona persona2 = persona
     | otherwise = persona2
 
+
+-- punto 9
 fingirDemencia :: Persona -> Fiesta -> Bool
 fingirDemencia persona = buenaFiesta . agregarSiNoEsta persona
 
